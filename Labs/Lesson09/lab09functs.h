443c0f4bdde9d222ab3433bf5e133c33c462378a
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-08-30 14:09:21 -0600

    lab09functs.h

---
+++ Labs/Lesson09/lab09functs.h
@@ -0,0 +1,27 @@
+/** lab09.c
+* ===========================================================
+* Name: Lauren Humpherys, 08/30/19
+* Section: M1A
+* Project: Lab 9 - Multiple Files, Debugging, and Testing
+* Purpose:
+* ===========================================================
+*/
+
+/**   ----------------------------------------------------------
+* <high level description of purpose of the function
+* including what the parameters are used for>
+* @param <explanation of each of the function parameters>
+* @return <explanation of what the function returns>
+*/
+
+#ifndef LAB09EXE_LAB09FUNCTS_H
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#define LAB09EXE_LAB09FUNCTS_H
+
+#endif //MYEXE_LAB09FUNCTS_H
+
+double volumeCylinder(int radius, int height);
+double volumeBox(int length, int width, int height1);
+double degToRad(int degrees);
