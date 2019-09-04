a3f8471375d5e5a145a0ed2ec3938683c87436f2
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-04 10:40:37 -0600

    lab10functs.h first commit

---
+++ Labs/Lesson10/lab10functs.h
@@ -0,0 +1,43 @@
+/** lab10functs.h
+* ===========================================================
+* Name: Lauren Humpherys, 9-4-19
+* Section: M1A
+* Project: Lab 10 - Switch Statements, Conditionals, and String Operations
+* Purpose:
+* ===========================================================
+*/
+#ifndef MYEXE_LAB10FUNCTS_H
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define MYEXE_LAB10FUNCTS_H
+#define MAGIC_CHAR '@'
+
+/**   ----------------------------------------------------------
+* <high level description of purpose of the function
+* including what the parameters are used for>
+* @param <explanation of each of the function parameters>
+* @return <explanation of what the function returns>
+*/
+bool isFirstHalfAlpha(int character);
+
+/**   ----------------------------------------------------------
+* <high level description of purpose of the function
+* including what the parameters are used for>
+* @param <explanation of each of the function parameters>
+* @return <explanation of what the function returns>
+*/
+int typeOfChar(char input);
+
+/**   ----------------------------------------------------------
+* <high level description of purpose of the function
+* including what the parameters are used for>
+* @param <explanation of each of the function parameters>
+* @return <explanation of what the function returns>
+*/
+bool isMagicChar(char magic);
+
+
+#endif //MYEXE_LAB10FUNCTS_H
