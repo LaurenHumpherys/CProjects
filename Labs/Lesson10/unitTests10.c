03aae9eb2818279e7a98e8dcdbf0168ec67f5b6b
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-06 07:53:41 -0600

    unitTests10.c final commit

---
+++ Labs/Lesson10/unitTests10.c
@@ -0,0 +1,20 @@
+/** unitTests10.c
+* ===========================================================
+* Name: Lauren Humpherys, 9-4-19
+* Section: M1A
+* Project: Lab 10 - Switch Statements, Conditionals, and String Operations
+* Purpose:
+* ===========================================================
+*/
+#include "lab10functs.h"
+
+int main()  {
+    assert(isFirstHalfAlpha('d'));
+
+    assert(typeOfChar('d'));
+
+    assert(isMagicChar('@'));
+
+
+}
+
