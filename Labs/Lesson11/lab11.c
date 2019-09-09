8e44700a64106659e728f6e24a0c41b3005939f7
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-08 23:48:45 -0600

    lab11.c third commit

--- Labs/Lesson11/lab11.c
+++ Labs/Lesson11/lab11.c
@@ -87,7 +87,7 @@ void floydsTriangle(int numRows)   {
                 printf("0");
             else
                 printf("1");
-            printf("\t");
+            printf("\n");
         }
         printf("\n");
     }
