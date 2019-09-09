9ff76332b55701a529e66bd647f0fdab5e8789f4
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-08 23:43:52 -0600

    lab11.h first commit

---
+++ Labs/Lesson11/lab11.h
@@ -0,0 +1,25 @@
+/** lab11.h
+* ===========================================================
+* Name: Name, Date
+* Section:
+* Project:
+* Purpose:
+* ===========================================================
+*/
+#ifndef MYEXE_LAB11_H
+#define MYEXE_LAB11_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int numFactorial(int input);
+
+int isPrime(int num);
+void floydsTriangle(int rows);
+
+
+
+#endif //MYEXE_LAB11_H
+
+
+
