1dd8ca0446b85cbbd4eeecacefa8b1a846863332
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-10 08:39:00 -0600

    lab12.h first commit

---
+++ Labs/Lesson12/lab12.h
@@ -0,0 +1,18 @@
+/** lab12.h
+* ===========================================================
+* Name: Name, Date
+* Section:
+* Project:
+* Purpose:
+* ===========================================================
+*/
+
+#ifndef MYEXE_LAB12_H
+#define MYEXE_LAB12_H
+
+#include <stdio.h>
+int isArmstrong(int num);
+int findGCD(int num1, int num2);
+void printRange(int lower, int upper);
+
+#endif //MYEXE_LAB12_H
