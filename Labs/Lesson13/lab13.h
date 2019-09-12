b34416ff53a816fc07a9ad5e86a428cae2770950
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-09-12 09:12:11 -0600

    lab13.h first commit

---
+++ Labs/Lesson13/lab13.h
@@ -0,0 +1,20 @@
+/** lab13.h
+* ===========================================================
+* Name: Name, Date
+        * Section:
+* Project:
+* Purpose:
+* ===========================================================
+*/
+
+#ifndef MYEXE_LAB13_H
+#define MYEXE_LAB13_H
+
+#include <stdio.h>
+
+void swapPassByValue(int classYear, int favNum);
+void swapPassByReference(int* val1, int*val2);
+int pointerSum(int* num1, int* num2);
+void findFactorial(int integer, long* longNum);
+
+#endif //MYEXE_LAB13_H
