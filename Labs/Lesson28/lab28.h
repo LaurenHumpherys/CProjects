644aa87df4f7dd2a606cd307726c5b459223495a
C22Lauren.Humpherys <C22Lauren.Humpherys@usafa.edu>
2019-10-28 23:21:44 -0600

    First commit

---
+++ Labs/Lesson28/lab28.h
@@ -0,0 +1,24 @@
+/** lab28.h
+* ===========================================================
+* Name: Lauren Humpherys, 28 Oct 2019
+* Section: M1A
+* Project: Programming Assessment 3 Practice
+* Documentation: DOC STATEMENT
+* ===========================================================  */
+
+#ifndef MYEXE_LAB28_H
+#define MYEXE_LAB28_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int getNumLines(char*);
+int numVowels(char* inputString);
+char* sortString(char* array);
+int readFile(char* filename, char* employeeData);
+
+
+
+#endif //MYEXE_LAB28_H
